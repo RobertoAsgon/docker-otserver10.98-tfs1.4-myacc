a0a85e78a700c8190015132298e06d7279945767
RobertoAssis <robertoasgon@gmail.com>
2024-09-08 15:52:35 -0300

    fix: docker

--- otsite/src/app/api/players/searchPlayer/service.ts
+++ otsite/src/app/api/players/searchPlayer/service.ts
@@ -1,9 +1,9 @@
 
-import pool from "@/libs/connectionDatabase";
+import mysql2Pool from "@/lib/mysql2Connection";
 import { RowDataPacket } from "mysql2";
 
 export async function findPlayersByName(name: string): Promise<RowDataPacket[]> {
-    const [rows] = await pool.query<RowDataPacket[]>(
+    const [rows] = await mysql2Pool.query<RowDataPacket[]>(
         `SELECT * FROM players WHERE name = ?`,
         [name]
     );
